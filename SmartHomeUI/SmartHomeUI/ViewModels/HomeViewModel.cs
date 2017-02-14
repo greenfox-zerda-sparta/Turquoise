964f2ef15d9de0aa14cae86e1f8d9c8d2a6b8c30
Ak0s <modis1akos@msn.com>
2017-02-14 14:05:09 +0100

    HomeView half-done

--- SmartHomeUI/SmartHomeUI/ViewModels/HomeViewModel.cs
+++ SmartHomeUI/SmartHomeUI/ViewModels/HomeViewModel.cs
@@ -2,27 +2,45 @@
 using System.Windows;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Windows.Threading;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace SmartHomeUI {
-  class HomeViewModel {
-    public ObservableCollection<Device> Devices { get; set; }
+    class HomeViewModel
+    {
+        public ObservableCollection<double> Consumptions { get; set; }
+        bool first_try = true;
 
-    public HomeViewModel() {
-      LoadDevices();
+        public HomeViewModel()
+        {
+            RefreshDatas();
+        }
+
+        private void RefreshDatas()
+        {
+            if (first_try == true)
+            {
+                getConsumptions();
+                first_try = false;
+            }
+            DispatcherTimer timer = new DispatcherTimer(DispatcherPriority.Background);
+            timer.Interval = TimeSpan.FromMinutes(0.5);
+            timer.IsEnabled = true;
+            timer.Tick += (s, e) => { Consumptions[0] = 4.1; };
+        }
+
+        public void getConsumptions()
+        {
+            ObservableCollection<double> consumptions = new ObservableCollection<double>();
+
+            consumptions.Add(3.9);
+            consumptions.Add(31.67);
+            consumptions.Add(613);
+
+            Consumptions = consumptions;
+        }
     }
-
-    public void LoadDevices() {
-      ObservableCollection<Device> devices = new ObservableCollection<Device>();
-
-      devices.Add(new Device { deviceID = "01", deviceType = "01", floor = "02", room = "01", status = "100", onOff = "01" });
-      devices.Add(new Device { deviceID = "01", deviceType = "03", floor = "03", room = "02", status = "23", onOff = "01" });
-      devices.Add(new Device { deviceID = "01", deviceType = "06", floor = "01", room = "01", status = "00", onOff = "01" });
-
-      Devices = devices;
-    }
-  }
 }

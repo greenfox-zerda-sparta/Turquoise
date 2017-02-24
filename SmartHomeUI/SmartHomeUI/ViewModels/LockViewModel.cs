9c6bd1e610ac534ace89ba86ff077461cae0db11
Ak0s <modis1akos@msn.com>
2017-02-24 12:22:00 +0100

    LockView Finished

--- SmartHomeUI/SmartHomeUI/ViewModels/LockViewModel.cs
+++ SmartHomeUI/SmartHomeUI/ViewModels/LockViewModel.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace SmartHomeUI
 {
     class LockViewModel
     {
+        public ICommand SetAlarmCommand { get; set; }
+        public LockViewModel()
+        {
+            SetAlarmCommand = new NavigationCommands(param => SetAlarm());
+        }
+
+        public void SetAlarm()
+        {
+            if (Instances.AllDevice[0].Status == 0)
+            {
+                Instances.AllDevice[0].Status = 1;
+            }
+            else if (Instances.AllDevice[0].Status == 1)
+            {
+                Instances.AllDevice[0].Status = 0;
+            }
+            (Instances.Models[(int)Models.InfoBar] as InfoBar).setAlarmStatus();
+        }
     }
 }
